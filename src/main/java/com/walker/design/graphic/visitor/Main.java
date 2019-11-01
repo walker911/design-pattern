ceddb33900f1fd1545d7abdeb80e076e594337dc
walker <qinmu911@163.com>
2019-11-01 16:08:25 +0800

    feat(graphic): add visitor pattern

---
+++ src/main/java/com/walker/design/graphic/visitor/Main.java
@@ -0,0 +1,11 @@
+package com.walker.design.graphic.visitor;
+
+/**
+ * @author walker
+ * @date 2019/11/1
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/walker/design/graphic/visitor/Visitor.java
@@ -0,0 +1,8 @@
+package com.walker.design.graphic.visitor;
+
+/**
+ * @author walker
+ * @date 2019/11/1
+ */
+public class Visitor {
+}
